f3ab92cf2addd1de6c0375cf1c7b3303f615210a
Caisin <kaixing622@qq.com>
2022-08-23 18:16:13 +0800

    feat: 加json tag

--- casbin/types.go
+++ casbin/types.go
@@ -1,8 +1,8 @@
 package casbin
 
 type Policy struct {
-	Sub         string //用户名或角色
-	ControlType string //控制类型
-	Res         string //资源地址
-	Action      string //访问方式
+	Sub         string `json:"sub"`         //用户名或角色
+	ControlType string `json:"controlType"` //控制类型
+	Res         string `json:"res"`         //资源地址
+	Action      string `json:"action"`      //访问方式
 }
